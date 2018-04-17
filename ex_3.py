b4f25256601166eeb0f26d70ce132a173686409f
YevheniiaSmyrnova <y.smyrnova@quantumobile.com>
2018-04-17 17:07:00 +0300

    temp

--- ex_3.py
+++ ex_3.py
@@ -5,6 +5,8 @@ import autograd.numpy as np
 from autograd import grad
 import autograd.numpy.random as npr
 
+from autograd.core import primitive
+
 from matplotlib import pyplot as plt
 
 
@@ -57,10 +59,10 @@ def sigmoid_grad(t):
     return sigmoid(t) * (1 - sigmoid(t))
 
 # input data
-n = 20
+n = 10
 T = []
 for i in range(0, n+1, 1):
-    T.append([1, 1, 1, i*(1./n)])
+    T.append([1, 1, 1, 1+i*(1./n)])
 T = np.array(T)
 # print T
 
@@ -73,10 +75,19 @@ X = np.array(X)
 np.random.seed(1)
 
 # 4 input and 1 output
-syn0 = 2 * np.random.random((4, 5)) - 1
-syn1 = 2 * np.random.random((5, 1)) - 1
+# syn0 = np.random.random((4, 5))
+# syn1 = np.random.random((5, 1))
 
-iter_count = 100000
+
+syn0 = npr.randn(4, 10)
+syn1 = npr.randn(10, 1)
+
+print syn0
+print syn1
+#
+# W = [npr.randn(1, 10), npr.randn(10, 1)]
+
+iter_count = 1000
 while True:
 
     l0 = T
@@ -85,19 +96,21 @@ while True:
     l2 = nonlin(np.dot(l1, syn1))
     l2_error = (X - l2)**2
 
-    if (iter_count % 10000) == 0:
+    if (iter_count % 100) == 0:
         print "Error:" + str(np.mean(np.abs(l2_error)))
+        # print l1
+        # print l2
 
     l2_delta = l2_error * nonlin(l2, deriv=True)
 
     l1_error = l2_delta.dot(syn1.T)
     l1_delta = l1_error * nonlin(l1, deriv=True)
 
-    syn1 -= l1.T.dot(l2_delta)
-    syn0 -= l0.T.dot(l1_delta)
+    syn1 += 0.001*l1.T.dot(l2_delta)
+    syn0 += 0.001*l0.T.dot(l1_delta)
 
     iter_count -= 1
-    print iter_count, np.mean(np.abs(l2_error)), l2
+    # print iter_count, np.mean(np.abs(l2_error)), l2
     if iter_count == 0:
         break
     elif np.mean(np.abs(l2_error)) <= 0.00000000002:
@@ -106,3 +119,4 @@ while True:
 
 print "Output:"
 print l2
+print X
